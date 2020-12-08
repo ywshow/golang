f56159b160797e4a5281ee520351534bffcf3bd8
yw <1530030957@qq.com>
2020-12-08 08:16:50 +0800

    字段更改

--- src/test/main/main.go
+++ src/test/main/main.go
@@ -9,9 +9,9 @@ import (
 func main() {
 	fmt.Println(">>>")
 	//结构体切片排序
-	slice := slice.RandSliceStudent(10)
-	sort.Sort(slice)
-	for _, v := range slice {
+	stu := slice.RandSliceStudent(10)
+	sort.Sort(stu)
+	for _, v := range stu {
 		fmt.Println(v)
 	}
 }
