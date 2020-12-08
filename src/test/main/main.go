5a821f80aac6cc94f0bb1f3bc9340a73b6e46b9e
yw <1530030957@qq.com>
2020-12-08 08:15:42 +0800

    结构体切片排序

--- src/test/main/main.go
+++ src/test/main/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+	"test/slice"
+)
 
 func main() {
 	fmt.Println(">>>")
+	//结构体切片排序
+	slice := slice.RandSliceStudent(10)
+	sort.Sort(slice)
+	for _, v := range slice {
+		fmt.Println(v)
+	}
 }
